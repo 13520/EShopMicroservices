a4842d631ff89216b47cefff5d17a0aedf400bba
Djordje Antonovic <djordjeantonovic@gmail.com>
2025-05-25 18:34:15 +0200

    GetProduct, all cases

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,30 @@
+﻿
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+    internal class GetProductByCategoryQueryHandler
+        (IDocumentSession session, ILogger<GetProductsQueryHandler> logger) 
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling GetProductByCategoryQuery. Handle called with {@Query}", query);
+
+            var products = await session.Query<Product>()
+                .Where(p => p.Categories.Contains(query.Category))
+                .ToListAsync(cancellationToken);
+
+            if(products is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
